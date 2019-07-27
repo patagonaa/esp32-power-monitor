301932fdfc3fd2208f4a44097cdaa8927084ae6c
Patrick Krämer <patrickkraemerhn@aol.com>
2019-07-27 18:51:50 +0200

    influxdb server

---
+++ server_influxdb/src/index.js
@@ -0,0 +1,65 @@
+const mqtt = require('mqtt');
+const request = require('request');
+
+const MQTT_SERVER = process.env.MQTT_SERVER || "mqtt://localhost";
+const MQTT_USER = process.env.MQTT_USER;
+const MQTT_PASSWORD = process.env.MQTT_PASSWORD;
+
+const INFLUX_SERVER = process.env.INFLUX_SERVER || "http://localhost:8086";
+const INFLUX_DATABASE = process.env.INFLUX_DATABASE || 'powermeter';
+
+function setWatthours(clientId, value) {
+    let requestOptions = {
+        body: `watthours_total,clientid=${clientId} value=${value}`
+    };
+    request.post(`${INFLUX_SERVER}/write?db=${INFLUX_DATABASE}`, requestOptions, (error, response, body) => {
+        if (error) {
+            console.error("error while transmitting value: ", error);
+        } else {
+            console.info('successfully transmitted value');
+        }
+    });
+}
+
+function setWatts(clientId, value) {
+    let requestOptions = {
+        body: `watts,clientid=${clientId} value=${value}`
+    };
+    request.post(`${INFLUX_SERVER}/write?db=${INFLUX_DATABASE}`, requestOptions, (error, response, body) => {
+        if (error) {
+            console.error("error while transmitting value: ", error);
+        } else {
+            console.info('successfully transmitted value');
+        }
+    });
+}
+
+const mqttClient = mqtt.connect(MQTT_SERVER, { username: MQTT_USER, password: MQTT_PASSWORD });
+
+mqttClient.on('connect', function () {
+    mqttClient.subscribe('powermeter/+/+');
+});
+
+mqttClient.on('message', function (topic, message) {
+    let topicSplit = topic.split('/');
+    let clientId = topicSplit[1];
+    let messageType = topicSplit[2];
+    switch (messageType) {
+        case 'watthours_total':
+            let wattHours = parseInt(message.toString());
+            console.info('Got', wattHours, 'watt hours from', clientId);
+            setWatthours(clientId, wattHours);
+            break;
+        case 'watts':
+            let watts = parseFloat(message.toString());
+            console.info('Got', watts, 'watts from', clientId);
+            setWatts(clientId, watts);
+            break;
+        case 'dead':
+            console.info('client', clientId, 'died with message', message.toString());
+            break;
+        default:
+            console.warn('unknown messageType', messageType);
+            break;
+    }
+});
